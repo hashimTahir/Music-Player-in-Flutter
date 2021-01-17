760da6920b133f416244f8b14ae08c1f2e55c9e4
hashim <hashimtahir@live.com>
2021-01-18 00:03:19 +0500

    Initial Commit.

---
+++ android/app/src/main/kotlin/com/hashim/music_player/MainActivity.kt
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2021/  1/ 17.  Created by Hashim Tahir 
+ */
+
+package com.hashim.music_player
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ integration_test/driver.dart
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021/  1/ 17.  Created by Hashim Tahir
+ */
+
+// This file is provided as a convenience for running integration tests via the
+// flutter drive command.
+//
+// flutter drive --driver integration_test/driver.dart --target integration_test/app_test.dart
+
+import 'package:integration_test/integration_test_driver.dart';
+
+Future<void> main() => integrationDriver();
