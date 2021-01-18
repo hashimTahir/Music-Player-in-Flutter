e3e0293ede98dd22a1278b7f9fa10baf938e1a05
hashim <hashimtahir@live.com>
2021-01-18 11:02:03 +0500

    Simple Splash page design.
    Signed-off-by: hashim <hashimtahir@live.com>

--- lib/pages/SplashPage.dart
+++ lib/pages/SplashPage.dart
@@ -11,12 +11,55 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+  var isLoading = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('SplashPage'),
-    );
+    return new Scaffold(
+        backgroundColor: Colors.deepPurple,
+        body: SafeArea(
+          child: new Container(
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      "images/kid.png",
+                      height: 200,
+                      width: 200,
+                      fit: BoxFit.cover,
+                    ),
+                  ],
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height / 5),
+                  child: Text(
+                    "Music player",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Center(
+                    child: isLoading
+                        ? CircularProgressIndicator(
+                            backgroundColor: Colors.white,
+                          )
+                        : Container(),
+                  ),
+                ),
+                Text("Setting up...",
+                    style: TextStyle(color: Colors.white, fontSize: 20))
+              ],
+            ),
+          ),
+        ));
   }
 }
-
-
