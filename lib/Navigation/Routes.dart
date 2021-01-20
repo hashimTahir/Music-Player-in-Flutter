654380271f178aab91aa48743226baa1ec4f16c5
hashim <hashimtahir@live.com>
2021-01-20 19:17:32 +0500

    Routes added for navigation.
    Signed-off-by: hashim <hashimtahir@live.com>

---
+++ lib/Navigation/Routes.dart
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2021/  1/ 20.  Created by Hashim Tahir
+ */
+
+class Routes {
+  static var hPlaylistRoute = '/hPlaylist';
+  static var hHomeRoute = '/hHome';
+  static var hSplashRoute = '/hSplash';
+  static var hSongDetailRoute = '/hSplash';
+}
