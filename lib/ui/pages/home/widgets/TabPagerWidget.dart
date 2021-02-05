445edf260e012fe4c9cd8142a15431a6351e20a4
hashim <hashimtahir@live.com>
2021-02-05 14:30:28 +0500

    Home page redesigned. Search bar added. Pager view added.
    Signed-off-by: hashim <hashimtahir@live.com>

--- lib/ui/pages/home/widgets/TabPagerWidget.dart
+++ lib/ui/pages/home/widgets/TabPagerWidget.dart
@@ -4,151 +4,93 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:material_floating_search_bar/material_floating_search_bar.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:music_player/utils/Constants.dart';
 
 class TabPagerWidget extends StatefulWidget {
   @override
   _TabPagerWidgetState createState() => _TabPagerWidgetState();
 }
 
-class _TabPagerWidgetState extends State<TabPagerWidget> {
+class _TabPagerWidgetState extends State<TabPagerWidget>
+    with SingleTickerProviderStateMixin {
+  TabController _hTabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _hTabController = TabController(length: 4, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _hTabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: hBuildAppBar(),
-        resizeToAvoidBottomInset: true,
-        body: Column(
-          children: [hBuildFloatingSearchBar(),hBuildTabController()],
-        ));
-  }
-
-  Widget hBuildTabController() {
-    return DefaultTabController(
-        length: 4,
-        child: Scaffold(
-          appBar: hBuildTabBar(),
-          resizeToAvoidBottomInset: true,
-          body: hBuildTabBarView(),
-        ));
+      appBar: hBuildAppBar(),
+      body: hBuidTabPages(),
+    );
   }
 
   Widget hBuildTabBar() {
-    return TabBar(
-      tabs: [
-        Text("Home"),
-        Text("Recent"),
-        Text("Albums"),
-        Text("Playlists"),
-      ],
+    return PreferredSize(
+      preferredSize: Size.fromHeight(10),
+      child: TabBar(
+        controller: _hTabController,
+        isScrollable: true,
+        tabs: [
+          Text("Home", style: hGetTabsTextStyle()),
+          Text("Recent", style: hGetTabsTextStyle()),
+          Text("Albums", style: hGetTabsTextStyle()),
+          Text("Playlists", style: hGetTabsTextStyle()),
+        ],
+      ),
     );
   }
 
-  Widget hBuildTabBarView() {
-    TabBarView(
-      children: [
-        Center(
-          child: Text("Home"),
-        ),
-        Center(
-          child: Text("Recent"),
-        ),
-        Center(
-          child: Text("Albums"),
-        ),
-        Center(
-          child: Text("Playlists"),
-        )
-      ],
-    );
-  }
-
-  TabBarView hBuildTabBarPages() {
-    return TabBarView(
-      children: [
-        Center(
-          child: Text("Home"),
-        ),
-        Center(
-          child: Text("Recent"),
-        ),
-        Center(
-          child: Text("Albums"),
-        ),
-        Center(
-          child: Text("Playlists"),
-        )
-      ],
-    );
+  TextStyle hGetTabsTextStyle() {
+    return TextStyle(color: Colors.deepOrange, fontSize: 18.0);
   }
 
   AppBar hBuildAppBar() {
     return AppBar(
       title: Text("Music Player"),
-      titleTextStyle: TextStyle(color: Colors.pink, fontSize: 20.0),
-
-      // bottom: TabBar(
-      //   tabs: [
-      //     Text("Home"),
-      //     Text("Recent"),
-      //     Text("Albums"),
-      //     Text("Playlists"),
-      //   ],
-      // ),
-    );
-  }
-
-  Widget hBuildFloatingSearchBar() {
-    return hBuildSearchBar();
-  }
-
-  Widget hBuildSearchBar() {
-    final isPortrait =
-        MediaQuery.of(context).orientation == Orientation.portrait;
-    return Expanded(
-      child: FloatingSearchBar(
-        hint: 'Search...',
-        scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
-        transitionDuration: const Duration(milliseconds: 800),
-        transitionCurve: Curves.easeInOut,
-        physics: const BouncingScrollPhysics(),
-        axisAlignment: isPortrait ? 0.0 : -1.0,
-        openAxisAlignment: 0.0,
-        maxWidth: isPortrait ? 600 : 500,
-        debounceDelay: const Duration(milliseconds: 500),
-        onQueryChanged: (query) {
-          // Call your model, bloc, controller here.
-        },
-        // Specify a custom transition to be used for
-        // animating between opened and closed stated.
-        transition: CircularFloatingSearchBarTransition(),
-        actions: [
-          FloatingSearchBarAction(
-            showIfOpened: false,
-            child: CircularButton(
-              icon: const Icon(Icons.search),
-              onPressed: () {},
-            ),
-          ),
-          FloatingSearchBarAction.searchToClear(
-            showIfClosed: false,
-          ),
-        ],
-        builder: (context, transition) {
-          return ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: Material(
-              color: Colors.white,
-              elevation: 4.0,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: Colors.accents.map((color) {
-                  return Container(height: 112, color: color);
-                }).toList(),
-              ),
-            ),
-          );
-        },
+      leading: SvgPicture.asset(
+          'images/headset.svg',
       ),
+      actions: [
+        IconButton(icon: Icon(Icons.search), onPressed: hOnSearchPressed)
+      ],
+      titleTextStyle: TextStyle(color: Colors.red, fontSize: 20.0),
+      bottom: hBuildTabBar(),
     );
+  }
+
+  Widget hBuidTabPages() {
+    return TabBarView(
+      controller: _hTabController,
+      children: [
+        Center(
+          child: Text("Home"),
+        ),
+        Center(
+          child: Text("Recent"),
+        ),
+        Center(
+          child: Text("Albums"),
+        ),
+        Center(
+          child: Text("Playlists"),
+        )
+      ],
+    );
+  }
+
+  void hOnSearchPressed() {
+    Constants.hLogger.d("Search Clicked");
   }
 }
